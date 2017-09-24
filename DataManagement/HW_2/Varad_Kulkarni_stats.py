bc62ebce837c4ab705a567ac0de21afd3f3f3613
Varad  Kulkarni <varad@Varads-MacBook-Pro.local>
2017-09-23 19:40:39 -0700

    .

--- DataManagement/HW_2/Varad_Kulkarni_stats.py
+++ DataManagement/HW_2/Varad_Kulkarni_stats.py
@@ -8,6 +8,12 @@ for x in list_of_tags:
     question_tags[x] = 0
 with open(input_file) as data_file:
     data = json.load(data_file)
-for x in data['data'][0]['paragraphs'][0]['qas']:
-    if x['question'].startswith('Which NFL'):
-        print x['question']
+len1 = len(data['data'])
+for x in range(len1):
+    for z in range(len(data['data'][x]['paragraphs'])):
+        for q in data['data'][x]['paragraphs'][z]['qas']:
+            for y in list_of_tags:
+                if q['question'].lower().startswith(y.lower()):
+                    question_tags[y]+=1
+                    print q['question']
+print question_tags
