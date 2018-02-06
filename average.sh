3856994f601cd7d3eb2b8fd038b9417a818b3504
Varad Kulkarni <varad2512@github.com>
2018-02-05 17:58:24 -0800

    Shell

---
+++ average.sh
@@ -0,0 +1,5 @@
+hadoop-2.7.5/bin/hadoop com.sun.tools.javac.Main Varad_Kulkarni_Average.java;
+jar cf Varad_Kulkarni_avg.jar Varad_Kulkarni_Average*.class;
+hadoop-2.7.5/bin/hadoop jar Varad_Kulkarni_avg.jar Varad_Kulkarni_Average input/menu output
+
+
